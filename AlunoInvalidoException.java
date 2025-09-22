ed24e7295992ea2b4ced5314545a8267e41dbc79
GabrielSRosendo <gabrielsrosendo@gmail.com>
2025-09-22 08:14:25 -0300

    Update AlunoInvalidoException.java

--- AlunoInvalidoException.java
+++ AlunoInvalidoException.java
@@ -3,5 +3,5 @@ package Projeto.gabriel.silva;
 public class AlunoInvalidoException extends Exception {
     public AlunoInvalidoException(String message) { super(message); }
 
-    public AlunoInvalidoException(){this("Perdão, mas o Aluno foi Invalido");}
+    public AlunoInvalidoException(){this("Perdão, mas o Aluno foi Invalido, ou não econtrado");}
 }
