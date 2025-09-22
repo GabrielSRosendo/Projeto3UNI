0195066efdaa5f9db99fbcdc62d94992eb184c91
GabrielSRosendo <gabrielsrosendo@gmail.com>
2025-09-22 08:13:04 -0300

    Update CadastroEstudanteCarteira.java

--- CadastroEstudanteCarteira.java
+++ CadastroEstudanteCarteira.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public interface CadastroEstudanteCarteira {
     void cadastrar(Estudante estudante);
-    Estudante buscarPorMatricula(int matricula) throws MatriculaInvalidaEception, AlunoInvalidoException;
+    Estudante buscarPorMatricula(long matricula) throws MatriculaInvalidaEception, AlunoInvalidoException;
     Estudante buscarPorCpf(String cpf) throws CpfInvalidoException, AlunoInvalidoException;
     boolean remover(String cpf) throws CpfInvalidoException, AlunoInvalidoException;
     List<Estudante> listarTodos();
